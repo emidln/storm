d844389ecd4ebd51cf35a3f91a1c72f224890b7d
Brandon Adams <emidln@gmail.com>
2015-05-27 17:04:39 -0500

    Remove unused clj-time dependency from storm-core
    clj-time was formerly used by UI, but is no longer. This removes
    clj-time (and thus joda-time 2.0). This might be a breaking change
    if a user assumes joda-time 2.0 or clj-time 0.8.0 is provided by
    the worker on the classpath.

--- storm-core/src/clj/backtype/storm/ui/helpers.clj
+++ storm-core/src/clj/backtype/storm/ui/helpers.clj
@@ -18,7 +18,6 @@
   (:use [hiccup core page-helpers])
   (:use [clojure [string :only [join]]])
   (:use [backtype.storm.util :only [uuid defnk url-encode]])
-  (:use [clj-time coerce format])
   (:import [backtype.storm.generated ExecutorInfo ExecutorSummary])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
@@ -124,7 +123,7 @@ $(\"table#%s\").each(function(i) { $(this).tablesorter({ sortList: %s, headers: 
      )))
 
 (defn url-format [fmt & args]
-  (String/format fmt 
+  (String/format fmt
     (to-array (map #(url-encode (str %)) args))))
 
 (defn to-tasks [^ExecutorInfo e]
